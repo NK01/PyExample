6d08355b285421ca6982d54e28177235f137365a
Nishant Kumar <52008914+NK01@users.noreply.github.com>
2022-01-19 11:56:24 +0900

    setup the module to be installable

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='PyExample',
+    version='0.1.0',    
+    description='A example Python package',
+    url='https://github.com/NK01/PyExample',
+    author='Nishant Kumar',
+    author_email='nishant.119@gmail.com',
+    license='MIT',
+    packages=['PyExample'],
+    install_requires=[ ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Everyone',
+    ],
+)
